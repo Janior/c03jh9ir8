499490d9e991c3a2eec5750996f133fbf200fa4a
Janior <janior_orlando@hotmail.com>
2018-08-11 17:33:54 -0700

    Lista de actores creada con EXITO

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,15 @@
 class ActorsController < ApplicationController
+   def listado
+       @actors=Actor.all
+   end
+
+   def new
+      @actor.Actor.new
+   end
+
+   def crear
+      @actor= Actor.new (params[:actor].permit(:name))
+      @actor.save 
+      redirect_to Actor_path
+   end
 end
